1e5fe973016b2e5e57e49ab28e05020c91756924
matardy <gutemberg.mendoza@epn.edu.ec>
2023-03-07 08:46:44 -0500

    first commit

--- main.py
+++ main.py
@@ -68,15 +68,15 @@ def receive_audio():
     audio_file = request.files['audio']
     audio = AudioSegment.from_file(audio_file)
     
-    # # Set the path and filename for the downloaded audio file
-    filename, extension = audio_file.filename.split('.')
-    filepath = 'audio/' + f'{filename}_downloaded.{extension}'
+    # # # Set the path and filename for the downloaded audio file
+    # filename, extension = audio_file.filename.split('.')
+    # filepath = 'audio/' + f'{filename}_downloaded.{extension}'
     
-    # # Write the audio file data to the downloaded file
-    audio.export(filepath, format=extension)
+    # # # Write the audio file data to the downloaded file
+    # audio.export(filepath, format=extension)
     
     
-    predict_function(filepath)
+    # #predict_function(filepath)
    
     response = jsonify({'message': 'Dani'})
     # response.hearders.add('Access-Control-Allow-Origin', '*')
